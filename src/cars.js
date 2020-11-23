5506eff7c4179fb4eb15a3e27fd2cbc9e8613721
Owais <precisework01@gmail.com>
2020-11-23 23:02:45 +0500

    session-03

---
+++ src/cars.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Cars(props) {
+    return (
+        <div>
+            <h1>Toyota has sedan name {props.carType}</h1>
+        </div>
+    )
+}
+
+export default Cars
