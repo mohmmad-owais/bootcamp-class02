e45125fc8103f5b68c868de41d5e3b068b227f66
Owais <precisework01@gmail.com>
2020-11-23 23:02:53 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 
 import './App.css';
+import Cars from './cars';
+
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World Updated</h1>
+      <Cars carType='Corolla'/>
     </div>
   );
 }
