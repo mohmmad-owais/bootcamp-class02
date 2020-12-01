f06bba31873d4302a0de0a64685615dc5dded12c
Owais <precisework01@gmail.com>
2020-12-01 16:24:43 +0500

    session-8

--- src/App.js
+++ src/App.js
@@ -1,12 +1,52 @@
-
+import React,{useState,useEffect}  from 'react'; 
 import './App.css';
 
+
 function App() {
+  
+
+  const [repos, setRepos]= useState([{}]);
+
+  useEffect(()=>{
+    
+    async function getRepos()
+    {
+      const response = await fetch ('https://api.github.com/users/mohmmad-owais/repos');
+      const data = await response.json();
+      console.log(data);
+      setRepos(data);
+
+    }
+
+    getRepos();
+    
+  
+  
+    //  fetch('https://jsonplaceholder.typicode.com/todos')
+  //   .then((response) => response.json())
+  //   .then((json) => {
+  //   console.log(json);
+  //   setData(json);
+  // })
+
+  },[])
+
   return (
-    <div className="App">
-      <h1>Hello World Updated</h1>
+
+    
+      <div>
+          <h2>My GitHub Repos </h2>
+          <ul>
+            {repos.map((repoObj,i)=>{
+              return (<li key={i}>{repoObj.name}</li>)
+            })}
+          </ul>
+
+
+
     </div>
-  );
+    
+  )
 }
 
 export default App;
